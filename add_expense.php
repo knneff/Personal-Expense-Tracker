b3d189dd94005ddd93579bd7ee59bc047da90e13
knmnltzy <knmnltzy@gmail.com>
2024-11-02 10:37:16 +0800

    fix the format

--- add_expense.php
+++ add_expense.php
@@ -85,23 +85,26 @@ if (isset($_GET['delete'])) {
     <meta name="author" content="">
 
     <title>Expense Manager - Dashboard</title>
-    
+
 
     <!-- Bootstrap core CSS -->
-        <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
 
-        <!-- Custom styles for this template -->
-        <link href="css/style.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- Feather JS for Icons -->
     <script src="js/feather.min.js"></script>
-<style>
-    .try {
-  font-size: 28px; /* Adjust the font size as needed */
-  color: #333;    /* Adjust the color as needed */
-  padding: 15px 70px 5px 0px;   /* Adjust the padding as needed */
-}
-</style>
+    <style>
+        .try {
+            font-size: 28px;
+            /* Adjust the font size as needed */
+            color: #333;
+            /* Adjust the color as needed */
+            padding: 15px 70px 5px 0px;
+            /* Adjust the padding as needed */
+        }
+    </style>
 </head>
 
 <body>
@@ -109,26 +112,26 @@ if (isset($_GET['delete'])) {
     <div class="d-flex" id="wrapper">
 
         <!-- Sidebar -->
-    <div class="border-right" id="sidebar-wrapper">
-      <div class="user">
-        <img class="img img-fluid rounded-circle" src="uploads\default_profile.png" width="120">
-        <h5><?php echo $username ?></h5>
-        <p><?php echo $useremail ?></p>
-      </div>
-      <div class="sidebar-heading">Management</div>
-      <div class="list-group list-group-flush">
-        <a href="index.php" class="list-group-item list-group-item-action"><span data-feather="home"></span> Dashboard</a>
-        <a href="add_expense.php" class="list-group-item list-group-item-action sidebar-active"><span data-feather="plus-square"></span> Add Expenses</a>
-        <a href="manage_expense.php" class="list-group-item list-group-item-action "><span data-feather="dollar-sign"></span> Manage Expenses</a>
-        <a href="expensereport.php" class="list-group-item list-group-item-action"><span data-feather="file-text"></span> Expense Report</a>
-      </div>
-      <div class="sidebar-heading">Settings </div>
-      <div class="list-group list-group-flush">
-        <a href="profile.php" class="list-group-item list-group-item-action "><span data-feather="user"></span> Profile</a>
-        <a href="logout.php" class="list-group-item list-group-item-action "><span data-feather="power"></span> Logout</a>
-      </div>
-    </div>
-    <!-- /#sidebar-wrapper -->
+        <div class="border-right" id="sidebar-wrapper">
+            <div class="user">
+                <img class="img img-fluid rounded-circle" src="uploads\default_profile.png" width="120">
+                <h5><?php echo $username ?></h5>
+                <p><?php echo $useremail ?></p>
+            </div>
+            <div class="sidebar-heading">Management</div>
+            <div class="list-group list-group-flush">
+                <a href="index.php" class="list-group-item list-group-item-action"><span data-feather="home"></span> Dashboard</a>
+                <a href="add_expense.php" class="list-group-item list-group-item-action sidebar-active"><span data-feather="plus-square"></span> Add Expenses</a>
+                <a href="manage_expense.php" class="list-group-item list-group-item-action "><span data-feather="dollar-sign"></span> Manage Expenses</a>
+                <a href="expensereport.php" class="list-group-item list-group-item-action"><span data-feather="file-text"></span> Expense Report</a>
+            </div>
+            <div class="sidebar-heading">Settings </div>
+            <div class="list-group list-group-flush">
+                <a href="profile.php" class="list-group-item list-group-item-action "><span data-feather="user"></span> Profile</a>
+                <a href="logout.php" class="list-group-item list-group-item-action "><span data-feather="power"></span> Logout</a>
+            </div>
+        </div>
+        <!-- /#sidebar-wrapper -->
 
         <!-- Page Content -->
         <div id="page-content-wrapper">
@@ -140,14 +143,14 @@ if (isset($_GET['delete'])) {
                     <span data-feather="menu"></span>
                 </button>
                 <div class="col-md-12 text-center">
-    <h3 class="try">Add Your Daily Expenses</h3>
-</div>
+                    <h3 class="try">Add Your Daily Expenses</h3>
+                </div>
 
-                <hr>                        
+                <hr>
             </nav>
 
             <div class="container">
-               
+
                 <div class="row ">
 
                     <div class="col-md-3"></div>
@@ -166,25 +169,25 @@ if (isset($_GET['delete'])) {
                                     <input type="date" class="form-control col-sm-12" value="<?php echo $expensedate; ?>" name="expensedate" id="expensedate" required>
                                 </div>
                             </div>
-<fieldset class="form-group">
-    <div class="row">
-        <label class="col-form-label col-sm-6 pt-0"><b>Category</b></label>
-        <div class="col-md">
-            <select class="form-control" id="expensecategory" name="expensecategory" required>
-                <?php
-                $categories_query = "SELECT * FROM expense_categories";
-                $categories_result = mysqli_query($con, $categories_query);
+                            <fieldset class="form-group">
+                                <div class="row">
+                                    <label class="col-form-label col-sm-6 pt-0"><b>Category</b></label>
+                                    <div class="col-md">
+                                        <select class="form-control" id="expensecategory" name="expensecategory" required>
+                                            <?php
+                                            $categories_query = "SELECT * FROM expense_categories";
+                                            $categories_result = mysqli_query($con, $categories_query);
 
-                while ($row = mysqli_fetch_assoc($categories_result)) {
-                    $category_name = $row['category_name'];
-                    $selected = ($category_name === $expensecategory) ? 'selected' : '';
-                    echo "<option value=\"$category_name\" $selected>$category_name</option>";
-                }
-                ?>
-            </select>
-        </div>
-    </div>
-</fieldset>
+                                            while ($row = mysqli_fetch_assoc($categories_result)) {
+                                                $category_name = $row['category_name'];
+                                                $selected = ($category_name === $expensecategory) ? 'selected' : '';
+                                                echo "<option value=\"$category_name\" $selected>$category_name</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                            </fieldset>
 
                             <div class="form-group row">
                                 <div class="col-md-12 text-right">
@@ -201,7 +204,7 @@ if (isset($_GET['delete'])) {
                     </div>
 
                     <div class="col-md-3"></div>
-                    
+
                 </div>
             </div>
         </div>
@@ -209,11 +212,11 @@ if (isset($_GET['delete'])) {
 
     </div>
     <!-- /#wrapper -->
-     <!-- Bootstrap core JavaScript -->
-  <script src="js/jquery.slim.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/Chart.min.js"></script>    
-  <script src="js/popper.min.js"></script>  
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/Chart.min.js"></script>
+    <script src="js/popper.min.js"></script>
     <!-- Menu Toggle Script -->
     <script>
         $("#menu-toggle").click(function(e) {
@@ -228,4 +231,5 @@ if (isset($_GET['delete'])) {
 
     </script>
 </body>
+
 </html>
